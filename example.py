6603c4791129e72b1dedfa75af3ccb8d516a0392
Facu <bruseghini.f@gmail.com>
2019-05-12 12:59:28 -0300

    Update example.py

--- example.py
+++ example.py
@@ -15,9 +15,35 @@ if __name__ == "__main__":
     
     words = 'the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog'
     seq = words.split()
-    
-    counts = spark.sparkContext.parallelize(seq).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b).collect()
-
+    print("HHOHOOHOHOHOHOHOHO")
+    print("HHOHOOHOHOHOHOHOHO")
+    print("HHOHOOHOHOHOHOHOHO")
+    print("HHOHOOHOHOHOHOHOHO")
+    data = spark.sparkContext.parallelize(seq)
+    print("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH")
+    print("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH")
+    print("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH")
+    print("HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAH")
+    counts = data.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b).collect()
+    print('HUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHGUHUHUHUHUHUHUHUHUHUHUHUHUHUHUH")
+    print('HUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHGUHUHUHUHUHUHUHUHUHUHUHUHUHUHUH")
+    print('HUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHGUHUHUHUHUHUHUHUHUHUHUHUHUHUHUH")
+    print('HUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHUHGUHUHUHUHUHUHUHUHUHUHUHUHUHUHUH")
     print("This is the result of the operation:")
-    print("dict(counts)")
+    dict(counts)
     sc.stop()
+
+#     -------------------------
+
+#     count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+#     print("Pi is roughly %f" % (4.0 * count / n))
+
+#     spark.stop()
+    
+    
+#     -------------------------
+
+#     data = sc.parallelize(seq)
+#     counts = data.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b).collect()
+#     dict(counts)
+#     sc.stop()
